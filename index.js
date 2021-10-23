d34acf9a8c822a142aee8f233b4ee30492a7614b
Daninogamer <ttnmdani@gmail.com>
2021-10-23 19:27:52 +0200

    start

---
+++ index.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const colors = require("colors")
+const client = new Discord.Client({
+  presence: {
+    status: "dnd",
+    activity: {
+      name: "I_Two_Dani",
+      type: "STREAMING",
+      url: "https://www.twitch.tv/i_two_dani"
+    }
+  }
+});
+client.config = require("./config.json");
+
+client.login(client.config.token);
+
+client.on("ready", ()=>{
+  console.log(`${client.user.tag} è online! Il prefisso è: ${client.config.prefix}`.bgGreen);
+  //client.user.setActivity("Hello world", {type: "STREAMING", url: "https://twitch.tv/#"})
+})
+
+require("./logger")(client);
+//const logger = require("./logger");
+//logger(client)
